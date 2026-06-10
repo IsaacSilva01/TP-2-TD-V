ffcaa9c888793f7c905f765831c6734eb99a52d2
IsaacSilva01 <silvaisaac327@gmail.com>
2026-06-10 14:20:55 -0300

    segundo intento de commit

--- tp2/src/instance.cpp
+++ tp2/src/instance.cpp
@@ -4,26 +4,40 @@ void GAPInstance::read(istream& in){
    if (!(in >> m >> n))
             cout<<("Error leyendo m y n")<< endl;
 
-        costo.assign(m, vector<int>(n));
-        demanda.assign(m, vector<int>(n));
+        costo.assign(m, vector<float>(n));
+        demanda.assign(m, vector<float>(n));
         capacidad.resize(m);
 
-        for (float i = 0; i < m; i++) 
-            for (float j = 0; j < n; j++) 
-                if (!(in >> costo[i][j])) cout<<("Error leyendo costos")<< endl;
-        
-        for (float i = 0; i < m; i++)
-            for (float j = 0; j < n; j++)
-                if (!(in >> demanda[i][j]))
-                    cout<<("Error leyendo demandas")<< endl;
-
-        for (float i = 0; i < m; i++)
-            if (!(in >> capacidad[i]))
-                cout<<("Error leyendo capacidad")<< endl; 
-}  
-void GAPInstance::readFromFile(const std::string& filename) {
-        std::ifstream f(filename);
-        if (!f.is_open())
-            cout<<("No se pudo abrir: " + filename)<<endl;
-        read(f);
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (!(in >> costo[i][j])) {
+                cout << "Error leyendo costos en posicion " << i << "," << j << endl;
+            }
+        }
     }
+    
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (!(in >> demanda[i][j])) {
+                cout << "Error leyendo demandas en posicion " << i << "," << j << endl;
+            }
+        }
+    }
+   
+    for (int i = 0; i < m; ++i) {
+        if (!(in >> capacidad[i])) {
+            cout << "Error leyendo capacidad del deposito " << i << endl; 
+        }
+    }
+
+}  
+
+void GAPInstance::readFromFile(const string& filename) {
+    ifstream f(filename);
+    if (!f.is_open()) {
+        cout << "No se pudo abrir el archivo: " << filename << endl;
+        return;
+    }
+    read(f);
+    f.close();
+}
