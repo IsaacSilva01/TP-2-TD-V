40b7cf477620408008350cb98fa3bda32e26fc55
Fran Goldstein <138630729+franixd@users.noreply.github.com>
2026-06-12 12:53:33 -0300

    comentarios

--- tp2/src/busqueda local/busq_l2.cpp
+++ tp2/src/busqueda local/busq_l2.cpp
@@ -10,18 +10,18 @@ void swap_busqueda_local(GAPSolution& sol, const GAPInstance& inst){
     while(mejoro){
         mejoro = false;
 
-        for (int j = 0; j < n && !mejoro; j++) {
-            int i = sol.asignacion[j];
+        for (int j = 0; j < n && !mejoro; j++) { // voy a recorrer los vendedores
+            int i = sol.asignacion[j]; // i va a ser el deposito en el que se encuentra el vendedor j
 
-            for(int h = 0; h < n && !mejoro; h++){
-                int q = sol.asignacion[h];
+            for(int h = 0; h < n && !mejoro; h++){ // vuelvo a recorrer los vendedores
+                int q = sol.asignacion[h]; // q va a ser el deposito en el que se encuentra el vendedor h
 
-                if(i != -1 && q != -1 && q != i &&
-                    inst.getdemanda(q, j) <= sol.capacidad_residual[q] + inst.getdemanda(q, h) &&
-                    inst.getdemanda(i, h) <= sol.capacidad_residual[i] + inst.getdemanda(i, j) &&
-                    inst.getcosto(q, j) + inst.getcosto(i, h) < inst.getcosto(i, j) + inst.getcosto(q, h)){
+                if(i != -1 && q != -1 && q != i && // me fijo que q e i sean depositos factibles y que sean distintos entre si 
+                    inst.getdemanda(q, j) <= sol.capacidad_residual[q] + inst.getdemanda(q, h) && // busco la demanda de j en q y me fijo si es menor a la capacidad restante de q (sacando el lugar que ocupa h en q)
+                    inst.getdemanda(i, h) <= sol.capacidad_residual[i] + inst.getdemanda(i, j) && // busco la demanda de h en i y me fijo si es menor a la capacidad restante de i (sacando el lugar que ocupa j en i)
+                    inst.getcosto(q, j) + inst.getcosto(i, h) < inst.getcosto(i, j) + inst.getcosto(q, h)){ // calculo los nuevos costos y me fijo si son menores a los anteriores
 
-                        sol.swap(j, h, inst);
+                        sol.swap(j, h, inst); // ejecuto el swap
                         mejoro = true;
                 }
             }
