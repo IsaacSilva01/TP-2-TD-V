5e01fc56bf96e8da53610894b7d2809bf27ae861
IsaacSilva01 <silvaisaac327@gmail.com>
2026-06-14 10:52:10 -0300

    greedy_randomizado comentado

--- tp2/src/heuristicas/greedy_random.cpp
+++ tp2/src/heuristicas/greedy_random.cpp
@@ -14,20 +14,20 @@ GAPSolution greedy_randomizado(const GAPInstance& inst, int k) {
     GAPSolution sol(inst);
 
     for (int j = 0; j < n; j++) {// recorro por vendedor
-        vector<pair<float, int>> candidatos; // creo un vector de pairs donde voy a guardar los posibles candidtaos
+        vector<pair<float, int>> candidatos; // creo un vector de pairs para el vendedor j donde voy a guardar un pair que dice {costo a deposito i, i}
         for( int i = 0; i < m ; i++){ // recorro los depositos
             if(sol.es_factible_asignar(j, i, inst)){ // me fijo si la demanda del vendedor es compatible con la capacidad del deposito
-                candidatos.push_back({inst.getcosto(i, j), i}); // agrego en la lista de posibles candidatos
+                candidatos.push_back({inst.getcosto(i, j), i}); // agrego en la lista de posibles candidatos del vendedor j
             }
 
         }
-        if(candidatos.empty()){ // si no hay posibles candidatos
-            sol.asignar(j, m, inst); // lo mando al final del vector asignar ya que no entra en ningun deposito
+        if(candidatos.empty()){ // si no encontre ningún depósito para el vendedor j
+            sol.asignar(j, -1, inst); // en la solución asigno j al deposito ficticio
         }
         else{
-            sort(candidatos.begin(), candidatos.end()); // ordeno los candidatos de menor costo a mayor costo
+            sort(candidatos.begin(), candidatos.end()); // ordeno los candidatos de menor costo a mayor costo. Primero por por el primer elemento (el costo), y si hay empate, por el segundo (el índice del depósito).
             int minimo = min(k, (int)candidatos.size()); // me guardo el minimo entre k y el tamaño de candidatos
-            vector<int>top_k; 
+            vector<int> top_k; 
             for (int t = 0; t < minimo; t++) { // recorro candidatos hasta el minimo qu eguarde anteriormente
                 top_k.push_back(candidatos[t].second); // me guardo en el vector top k los numeros de los mejores depositos en orden
             }
